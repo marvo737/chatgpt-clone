819ea5e75434cc32073e2f3ce8f51e3e21bb0155
Matsuzaki Masahiro <marvo.develop@gmail.com>
2023-04-07 16:03:56 +0900

    supported chatGPT API

--- app.py
+++ app.py
@@ -3,32 +3,19 @@ import openai
 import gradio as gr
 
 #if you have OpenAI API key as an environment variable, enable the below
-#openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 #if you have OpenAI API key as a string, enable the below
-openai.api_key = "xxxxxx"
-
-start_sequence = "\nAI:"
-restart_sequence = "\nHuman: "
-
-prompt = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: "
+#openai.api_key = ""
 
 def openai_create(prompt):
-
-    response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=prompt,
-    temperature=0.9,
-    max_tokens=150,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0.6,
-    stop=[" Human:", " AI:"]
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "user", "content": prompt},
+    ],
     )
-
-    return response.choices[0].text
-
-
+    return response.choices[0].message.content
 
 def chatgpt_clone(input, history):
     history = history or []
@@ -39,15 +26,13 @@ def chatgpt_clone(input, history):
     history.append((input, output))
     return history, history
 
-
 block = gr.Blocks()
-
 
 with block:
     gr.Markdown("""<h1><center>Build Yo'own ChatGPT with OpenAI API & Gradio</center></h1>
     """)
     chatbot = gr.Chatbot()
-    message = gr.Textbox(placeholder=prompt)
+    message = gr.Textbox(placeholder="話したい内容を入力してください")
     state = gr.State()
     submit = gr.Button("SEND")
     submit.click(chatgpt_clone, inputs=[message, state], outputs=[chatbot, state])
